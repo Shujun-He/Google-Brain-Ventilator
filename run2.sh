05a14b70aa71a325b2a16448095e4c36944a989c
Shujun He <ukrakim@gmail.com>
2021-11-07 16:16:34 -0800

    deleted run1.sh and run2.sh

--- run2.sh
+++
@@ -1,5 +0,0 @@
-for i in {6..9};do
-  python run.py --fold $i --nfolds 10 --epochs 150 --embed_dim 256 --nheads 16 --nlayers 3 --rnnlayers 3\
-  --gpu_id 1 \
-  --pos_encode LSTM --seed 2020 --nfeatures 50 --batch_size 128 --path ../.. --workers 8 --dropout 0
-done
